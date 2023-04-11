75edc576448dbc2e7582883516a190faeaa89d07
Aleksandr <aleksvarlaam@gmail.com>
2023-04-11 16:24:41 +0300

    Fixed comments controller and form

--- app/controllers/feedbacks/comments_controller.rb
+++ app/controllers/feedbacks/comments_controller.rb
@@ -16,43 +16,51 @@ module Feedbacks
       # Set object of comment
       @new_comment.object = @new_comment.commentable.instance_of?(Comment) ? @commentable.object : @commentable
 
-      return unless @new_comment.save
-
-      if @new_comment.commentable.instance_of?(Comment)
-        @new_comment.broadcast_append_to @new_comment.commentable,
-                                         target: helpers.dom_id(@new_comment.commentable, :replies),
-                                         partial: 'feedbacks/comments/comment', locals: { comment: @new_comment.decorate }
+      if @new_comment.save
+        if @new_comment.commentable.instance_of?(Comment)
+          @new_comment.broadcast_append_to @new_comment.commentable,
+                                           target: helpers.dom_id(@new_comment.commentable, :replies),
+                                           partial: 'feedbacks/comments/comment', locals: { comment: @new_comment.decorate }
+        else
+          @new_comment.broadcast_prepend_to @commentable,
+                                            target: helpers.dom_id(@commentable, :comments),
+                                            partial: 'feedbacks/comments/comment', locals: { comment: @new_comment.decorate }
+        end
       else
-        @new_comment.broadcast_prepend_to @commentable,
-                                          target: helpers.dom_id(@commentable, :comments),
-                                          partial: 'feedbacks/comments/comment', locals: { comment: @new_comment.decorate }
+        respond_to do |format|
+          format.turbo_stream { flash.now[:alert] = t('flash.alert') }
+        end
       end
     end
 
     def edit; end
 
     def update
-      return unless current_user.comments.include?(@comment)
-      return unless @comment.update(comment_params)
-
-      @comment.broadcast_update_to @comment.commentable,
-                                   target: helpers.dom_id(@comment),
-                                   partial: 'feedbacks/comments/comment', locals: { comment: @comment.decorate }
       respond_to do |format|
-        format.turbo_stream { flash.now[:success] = t('flash.success.updated', model: @comment.model_name.human.to_s) }
+        if current_user.comments.include?(@comment) && @comment.update(comment_params)
+
+          @comment.broadcast_update_to @comment.commentable,
+                                       target: helpers.dom_id(@comment),
+                                       partial: 'feedbacks/comments/comment', locals: { comment: @comment.decorate }
+        
+          format.turbo_stream { flash.now[:success] = t('flash.success.updated', model: @comment.model_name.human.to_s) }
+        else
+          format.turbo_stream { flash.now[:alert] = t('flash.alert') }
+        end
       end
     end
 
     def destroy
-      return unless current_user.comments.include?(@comment) || current_admin
-      return unless @comment.destroy
-
-      @comment.broadcast_remove_to @comment.commentable,
-                                   target: helpers.dom_id(@comment)
-
       respond_to do |format|
-        format.turbo_stream do
-          flash.now[:success] = t('flash.success.destroyed', model: @comment.model_name.human.to_s)
+        if (current_user.comments.include?(@comment) || current_admin) && @comment.destroy
+
+          @comment.broadcast_remove_to @comment.commentable,
+                                       target: helpers.dom_id(@comment)
+
+        
+          format.turbo_stream { flash.now[:success] = t('flash.success.destroyed', model: @comment.model_name.human.to_s)}
+        else
+          format.turbo_stream { flash.now[:alert] = t('flash.alert') }
         end
       end
     end
@@ -64,11 +72,17 @@ module Feedbacks
     end
 
     def set_commentable
-      @commentable = Company.find_by_id(params[:company_id]) if params[:company_id].present?
-      @commentable = Truck.find_by_id(params[:truck_id])     if params[:truck_id].present?
-      @commentable = News.find_by_id(params[:news_id])       if params[:news_id].present?
-      @commentable = Comment.find_by_id(params[:comment_id]) if params[:comment_id].present?
-      @commentable = Statistic.find_by_id(params[:statistic_id]) if params[:statistic_id].present?
+      if params[:company_id].present?
+        @commentable = Company.find_by_id(params[:company_id])
+      elsif params[:truck_id].present?
+        @commentable = Truck.find_by_id(params[:truck_id])     
+      elsif params[:news_id].present?  
+        @commentable = News.find_by_id(params[:news_id])       
+      elsif params[:comment_id].present?
+        @commentable = Comment.find_by_id(params[:comment_id]) 
+      else
+        @commentable = Statistic.first
+      end
     end
 
     def set_comment
