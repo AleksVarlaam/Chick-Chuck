c0e86f01021567437afcf180b6e4004d1c31a6c5
Aleksandr <aleksvarlaam@gmail.com>
2023-06-13 21:39:04 +0500

    Updated OG meta tags

--- app/helpers/meta_tags_helper.rb
+++ app/helpers/meta_tags_helper.rb
@@ -3,6 +3,7 @@
 module MetaTagsHelper
   def default_meta_tags
     {
+      charset:  'utf-8',
       viewport: 'width=device-width,initial-scale=1',
       site: 'Chick-Chuck.com',
       reverse: true,
@@ -10,10 +11,19 @@ module MetaTagsHelper
       keywords: "#{t('company.carriers')}, #{t('moving_preparation.title')}, #{t('moving')}, #{t('pages.market')}",
       canonical: request.original_url,
       noindex: !Rails.env.production?,
+      manifest: '/manifest.webmanifest',
       icon: [
-        { href: image_url(asset_path('icons/chick-chuck/chick-chuck-logo.png')) },
-        { href: image_url(asset_path('icons/chick-chuck/chick-chuck-logo.png')), rel: 'apple-touch-icon',
-          sizes: '180x180', type: 'image/jpg' }
+        { 
+          href: image_url(asset_path('favicon.ico')), sizes: 'any'
+        },
+        { 
+          href: image_url(asset_path('android-chrome-512x512.png')), rel: 'icon',
+          sizes: '512x512', type: 'image/png' 
+        },
+        { 
+          href: image_url(asset_path('apple-touch-icon.png')), rel: 'apple-touch-icon',
+          sizes: '180x180', type: 'image/png' 
+        }
       ],
       og: {
         title: :title,
@@ -21,7 +31,13 @@ module MetaTagsHelper
         description: :description,
         type: 'website',
         url: request.original_url,
-        image: image_url(asset_path('icons/chick-chuck/chick-chuck-logo.png'))
+        image: {
+          url: image_url(asset_path('icons/chick-chuck/og-chick-chuck.png')),
+          type: 'image/png'
+        }
+      },
+      fb: {
+        app_id: '2318377664996656'
       },
       author: 'Aleksandr Varlamov',
       'google-site-verification' => 'google-site-verification=toUojvLJpY342LZjZXWqyfs2Mw4ne0xuoljh2V1QgHw',
