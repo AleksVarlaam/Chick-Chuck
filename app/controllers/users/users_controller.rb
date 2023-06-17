0e41ea4d1f2881924b3a39f6e457341502402854
Aleksandr <aleksvarlaam@gmail.com>
2023-06-17 14:34:26 +0500

    Updated meta

--- app/controllers/users/users_controller.rb
+++ app/controllers/users/users_controller.rb
@@ -8,8 +8,12 @@ module Users
     after_action :update_views, only: :show
 
     def index
+      title_district = District.find(params[:district_id].to_i).decorate.title if params[:district_id].present?
+      title_language = Language.model_name.human + '-' + Language.find(params[:language_id].to_i).title if params[:language_id].present?
+      title_israel   = t('israel') if title_district.blank? || title_language.blank?
+      
       set_meta_tags( 
-        title: t('meta.carriers.title'),
+        title: [title_language, title_israel, title_district, t('meta.carriers.title')],
         description: t('meta.carriers.desc')
       )
       
@@ -38,9 +42,20 @@ module Users
       @pagy, @reviews = pagy(@reviews, items: 10, fragment: '#reviews')
     end
 
-    def modal; end
+    def modal
+      set_meta_tags( 
+        title: [t('user_menu.profile'), @user.user_name.capitalize],
+        description: ("#{@user.user_name.capitalize} | #{t('user_menu.profile')}"),
+        noindex: true
+      )
+    end
 
     def contacts
+      set_meta_tags( 
+        title: [t('contacts'), @user.user_name.capitalize],
+        description: ("#{@user.user_name.capitalize} | #{t('contacts')}"),
+        noindex: true
+      )
       # return redirect_to new_client_session_path, alert: t('devise.failure.unauthenticated') unless user_signed_in?
     end
 
