285ac85ae8edc20e14c151c91c9c175c3fccb38d
Aleksandr <aleksvarlaam@gmail.com>
2023-06-20 22:53:51 +0500

    Fixed review new and product new path

--- app/helpers/feedbacks/reviews_helper.rb
+++ app/helpers/feedbacks/reviews_helper.rb
@@ -18,7 +18,7 @@ module Feedbacks
                 class: 'text-white bg-gradient-to-r from-blue-500 via-blue-600 to-blue-700 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center h-10 flex items-center',
                 data: { turbo_frame: 'modal' })
       else
-        link_to(t('review.add'), new_review_path(company_id: company.id),
+        link_to(t('review.add'), client_signed_in? ? new_review_path(company_id: company.id) : new_client_session_path,
                 class: 'text-white bg-gradient-to-r from-blue-500 via-blue-600 to-blue-700 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center h-10 flex items-center',
                 data: { turbo_frame: 'modal' })
       end
