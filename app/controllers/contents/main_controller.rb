1f4a5c60c6c3a11655e93e0ca789942cbae04875
Aleksandr <aleksvarlaam@gmail.com>
2023-06-28 21:25:21 +0500

    Fixed root canonical page

--- app/controllers/contents/main_controller.rb
+++ app/controllers/contents/main_controller.rb
@@ -5,7 +5,8 @@ module Contents
     def index
       set_meta_tags(
         title: [params[:locale]&.capitalize, t('meta.home_page.title')],
-        description: t('meta.home_page.desc')
+        description: t('meta.home_page.desc'),
+        canonical: I18n.locale.to_s == params[:locale].to_s || I18n.default_locale.to_s ? root_url(locale: I18n.locale) : request.original_url,
       )
       @companies = Company.confirmed.take(3)
       @products = Product.where(published: true).decorate.take(6)
