c72d2f2024fa5149ffc1297642142abfbb7cafb3
Aleksandr <aleksvarlaam@gmail.com>
2023-07-31 12:07:40 +0500

    Refactoring

--- app/controllers/concerns/analytics.rb
+++ app/controllers/concerns/analytics.rb
@@ -8,7 +8,7 @@ module Analytics
 
     def record_page_view
       # This condition should skip bots.
-      return if request.is_crawler? || admin_signed_in?
+      return if request.is_crawler? || user_signed_in?
 
       ActiveAnalytics.record_request(request)
     end
